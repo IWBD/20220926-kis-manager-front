9c25d0b40a81be2b020e03bf0b3718ce8c1785ce
C:\workspace <wjdruf23123@gmail.com>
2022-09-26 21:44:36 +0900

    프로젝트 초기 폴더 구조 생성

---
+++ source/src/app/index.tsx
@@ -0,0 +1,13 @@
+import react from 'react'
+import Axios from 'axios'
+
+const App = () => {
+  console.log( Axios )
+  return (
+    <div >
+      hello word
+    </div>
+  )
+}
+
+export default App
